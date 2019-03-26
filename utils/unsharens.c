8c9777b7bae2c267b0d38a888fe03e80f88953dc
Jonathan Calmels <jcalmels@nvidia.com>
2019-03-26 13:27:31 -0700

    Ignore all the syscalls related to uid/gid manipulation

--- utils/unsharens.c
+++ utils/unsharens.c
@@ -5,12 +5,22 @@
 #define _GNU_SOURCE
 #include <err.h>
 #include <errno.h>
+#include <linux/audit.h>
+#include <linux/filter.h>
+#include <linux/seccomp.h>
 #include <sched.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/prctl.h>
+#include <sys/syscall.h>
 #include <unistd.h>
 
 #include "common.h"
+
+#ifndef __x86_64__
+# error "unsupported architecture"
+#endif
 
 #ifndef PR_CAP_AMBIENT
 # define PR_CAP_AMBIENT 47
@@ -23,6 +33,39 @@
 #endif
 
 static struct capabilities_v3 caps;
+
+static struct sock_filter filter[] = {
+        /* Check for the syscall architecture (x86_64 and x32 ABIs). */
+        BPF_STMT(BPF_LD|BPF_W|BPF_ABS, offsetof(struct seccomp_data, arch)),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, AUDIT_ARCH_X86_64, 1, 0),
+        BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_KILL),
+
+        /* Load the syscall number. */
+        BPF_STMT(BPF_LD|BPF_W|BPF_ABS, offsetof(struct seccomp_data, nr)),
+
+        /* Return success on all the following syscalls. */
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setuid,    15, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setgid,    14, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setreuid,  13, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setregid,  12, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setresuid, 11, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setresgid, 10, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setgroups, 9,  0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fchownat,  8,  0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_chown,     7,  0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fchown,    6,  0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_lchown,    5,  0),
+
+        /* For setfsuid/setfsgid we only return success if the uid/gid argument is not -1. */
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setfsuid, 1, 0),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_setfsgid, 0, 2),
+        BPF_STMT(BPF_LD|BPF_W|BPF_ABS, offsetof(struct seccomp_data, args[0])),
+        BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, (uint32_t)-1, 0, 1),
+
+        /* Execute the syscall as usual otherwise. */
+        BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_ALLOW),
+        BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_ERRNO|(SECCOMP_RET_DATA & 0x0)),
+};
 
 int
 main(int argc, char *argv[])
@@ -62,6 +105,9 @@ main(int argc, char *argv[])
                         }
                 }
         }
+
+        if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &(const struct sock_fprog){ARRAY_SIZE(filter), filter}) < 0)
+                err(EXIT_FAILURE, "failed to register seccomp filter");
 
         if (execvp(argv[1], &argv[1]) < 0)
                 err(EXIT_FAILURE, "failed to execute: %s", argv[1]);
