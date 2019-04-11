d36add5307f6b844a2aa0ecc3fa6b1046e8e41d3
Jonathan Calmels <jcalmels@nvidia.com>
2019-04-10 17:45:09 -0700

    Tweak the autorc hook to allow enroot.rc as well as <rootfs_prefix>.rc

--- conf/hooks/20-autorc.sh
+++ conf/hooks/20-autorc.sh
@@ -8,10 +8,10 @@ source "${ENROOT_LIBEXEC_PATH}/common.sh"
 
 common::ckcmd grep find
 
-readonly prefix="$(basename "${ENROOT_ROOTFS}" | grep -o "^[[:alnum:]_-]\+")"
+readonly prefix="$(basename "${ENROOT_ROOTFS}" | grep -o "^[[:alnum:]_]\+")"
 
 if [ -n "${prefix}" ]; then
-    find -maxdepth 1 -type f ! -empty -name "${prefix}.rc" -exec echo \
+    find -maxdepth 1 -type f ! -empty \( -name "${prefix}.rc" -o -name "enroot.rc" \) -exec echo \
       {} /etc/rc none bind,x-create=file,nofail,silent \; \
       | "${ENROOT_LIBEXEC_PATH}/mountat" --root "${ENROOT_ROOTFS}" -
 fi
