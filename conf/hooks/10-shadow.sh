56fb5501a09abdd69300aedc2ac5d19b3a7746d8
Jonathan Calmels <jcalmels@nvidia.com>
2019-05-22 17:04:43 -0700

    Protect shadow databases with locks

--- conf/hooks/10-shadow.sh
+++ conf/hooks/10-shadow.sh
@@ -7,7 +7,7 @@ set -eu
 # shellcheck disable=SC1090
 source "${ENROOT_LIBRARY_PATH}/common.sh"
 
-common::checkcmd awk getent sed grpck pwck
+common::checkcmd awk getent sed grpck pwck flock
 
 readonly nobody=$(< /proc/sys/kernel/overflowuid)
 readonly nogroup=$(< /proc/sys/kernel/overflowgid)
@@ -34,41 +34,47 @@ if [ ! -x "${ENROOT_ROOTFS}${shell}" ]; then
     shell="${def_SHELL:-/bin/sh}"
 fi
 
-# Create new database files based on the ones present in the rootfs.
-cp -a "${ENROOT_ROOTFS}/etc/passwd" "${ENROOT_ROOTFS}/etc/passwd-"
-cp -a "${ENROOT_ROOTFS}/etc/group" "${ENROOT_ROOTFS}/etc/group-"
+(
+    flock -w 5 "${pwdlock}" || common:err "Could not acquire passwd lock"
+    flock -w 5 "${grplock}" || common:err "Could not acquire group lock"
 
-# XXX On debian based distributions, remove the _apt user/group otherwise apt will try to drop privileges and fail.
-if [ -f "${ENROOT_ROOTFS}/etc/debian_version" ]; then
-    sed -i '/^_apt:/d' "${ENROOT_ROOTFS}/etc/passwd-"
-    sed -i '/^_apt:/d' "${ENROOT_ROOTFS}/etc/group-"
-fi
+    # Create new database files based on the ones present in the rootfs.
+    cp -a "${ENROOT_ROOTFS}/etc/passwd" "${ENROOT_ROOTFS}/etc/passwd-"
+    cp -a "${ENROOT_ROOTFS}/etc/group" "${ENROOT_ROOTFS}/etc/group-"
 
-# Generate user entries for root, nobody and the current user.
-cat << EOF >> "${ENROOT_ROOTFS}/etc/passwd-"
-root:x:0:0:root:/root:${shell}
-nobody:x:${nobody}:${nogroup}:nobody:/:/sbin/nologin
-${user}:x:${uid}:${gid}:${gecos}:${home}:${shell}
-EOF
+    # XXX On debian based distributions, remove the _apt user/group otherwise apt will try to drop privileges and fail.
+    if [ -f "${ENROOT_ROOTFS}/etc/debian_version" ]; then
+        sed -i '/^_apt:/d' "${ENROOT_ROOTFS}/etc/passwd-"
+        sed -i '/^_apt:/d' "${ENROOT_ROOTFS}/etc/group-"
+    fi
 
-# Generate group entries for root, nobody and the current user.
-cat << EOF >> "${ENROOT_ROOTFS}/etc/group-"
-root:x:0:
-nogroup:x:${nogroup}:
-${group}:x:${gid}:
-EOF
+    # Generate user entries for root, nobody and the current user.
+    cat <<- EOF >> "${ENROOT_ROOTFS}/etc/passwd-"
+	root:x:0:0:root:/root:${shell}
+	nobody:x:${nobody}:${nogroup}:nobody:/:/sbin/nologin
+	${user}:x:${uid}:${gid}:${gecos}:${home}:${shell}
+	EOF
 
-# Check and install the new group database making sure our generated groups come first.
-yes | grpck -R "${ENROOT_ROOTFS}" /etc/group- > /dev/null 2>&1 || :
-tail -n -3 "${ENROOT_ROOTFS}/etc/group-" > "${ENROOT_ROOTFS}/etc/group"
-head -n -3 "${ENROOT_ROOTFS}/etc/group-" >> "${ENROOT_ROOTFS}/etc/group"
+    # Generate group entries for root, nobody and the current user.
+    cat <<- EOF >> "${ENROOT_ROOTFS}/etc/group-"
+	root:x:0:
+	nogroup:x:${nogroup}:
+	${group}:x:${gid}:
+	EOF
 
-# Check and install the new user database making sure our generated users come first.
-yes | pwck -R "${ENROOT_ROOTFS}" /etc/passwd- > /dev/null 2>&1 || :
-tail -n -3 "${ENROOT_ROOTFS}/etc/passwd-" > "${ENROOT_ROOTFS}/etc/passwd"
-head -n -3 "${ENROOT_ROOTFS}/etc/passwd-" >> "${ENROOT_ROOTFS}/etc/passwd"
+    # Check and install the new group database making sure our generated groups come first.
+    yes | grpck -R "${ENROOT_ROOTFS}" /etc/group- > /dev/null 2>&1 || :
+    tail -n -3 "${ENROOT_ROOTFS}/etc/group-" > "${ENROOT_ROOTFS}/etc/group"
+    head -n -3 "${ENROOT_ROOTFS}/etc/group-" >> "${ENROOT_ROOTFS}/etc/group"
 
-rm -f "${ENROOT_ROOTFS}/etc/group-" "${ENROOT_ROOTFS}/etc/passwd-"
+    # Check and install the new user database making sure our generated users come first.
+    yes | pwck -R "${ENROOT_ROOTFS}" /etc/passwd- > /dev/null 2>&1 || :
+    tail -n -3 "${ENROOT_ROOTFS}/etc/passwd-" > "${ENROOT_ROOTFS}/etc/passwd"
+    head -n -3 "${ENROOT_ROOTFS}/etc/passwd-" >> "${ENROOT_ROOTFS}/etc/passwd"
+
+    rm -f "${ENROOT_ROOTFS}/etc/group-"* "${ENROOT_ROOTFS}/etc/passwd-"*
+
+) {pwdlock}>> "${ENROOT_ROOTFS}/etc/passwd" {grplock}>> "${ENROOT_ROOTFS}/etc/group"
 
 # Create the user home directory if it doesn't exist and populate it with the content of the skeleton directory.
 if [ ! -e "${ENROOT_ROOTFS}${home}" ] && [ "${def_CREATE_HOME:-yes}" = "yes" ]; then
