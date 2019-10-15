54c99ef7ac2041bfd54890d2e30ad2df55464009
Jonathan Calmels <jcalmels@nvidia.com>
2019-10-14 19:11:57 -0700

    Add support for GDRCopy

--- conf/hooks/98-nvidia.sh
+++ conf/hooks/98-nvidia.sh
@@ -70,4 +70,9 @@ if ! grep -q nvidia_uvm /proc/modules; then
     common::log WARN "Kernel module nvidia_uvm is not loaded. Make sure the NVIDIA device driver is installed and loaded."
 fi
 
+# XXX Add support for GDRCopy since libnvidia-container doesn't support it yet.
+if [[ " ${cli_args[@]} " =~ " --compute " ]]; then
+    enroot-mount --root "${ENROOT_ROOTFS}" - <<< "/dev/gdrdrv /dev/gdrdrv none x-create=file,bind,ro,nosuid,noexec,nofail,silent"
+fi
+
 exec nvidia-container-cli --user ${NVIDIA_DEBUG_LOG+--debug=/dev/stderr} configure "${cli_args[@]}" "${ENROOT_ROOTFS}"
