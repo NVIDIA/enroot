aabef9deed587eeddcb4e8bae85ec9e2fa44c2b7
Jonathan Calmels <jcalmels@nvidia.com>
2019-10-28 14:40:16 -0700

    Create a new devpts instance when relying on ENROOT_RESTRICT_DEV

--- conf/hooks/10-devices.sh
+++ conf/hooks/10-devices.sh
@@ -20,6 +20,9 @@ if [ -z "${ENROOT_RESTRICT_DEV-}" ]; then
     exit 0
 fi
 
+# shellcheck disable=SC1090
+source "${ENROOT_LIBRARY_PATH}/common.sh"
+
 cat << EOF | enroot-mount --root "${ENROOT_ROOTFS}" -
 none                    /dev            none    x-detach,nofail,silent
 tmpfs                   /dev            tmpfs   x-create=dir,rw,nosuid,noexec,mode=755
@@ -28,17 +31,20 @@ tmpfs                   /dev            tmpfs   x-create=dir,rw,nosuid,noexec,mo
 /dev/full               /dev/full       none    x-create=file,bind,rw,nosuid,noexec
 /dev/urandom            /dev/urandom    none    x-create=file,bind,rw,nosuid,noexec
 /dev/random             /dev/random     none    x-create=file,bind,rw,nosuid,noexec
-/dev/pts                /dev/pts        none    x-create=dir,bind,rw,nosuid,noexec
-/dev/ptmx               /dev/ptmx       none    x-create=file,bind,rw,nosuid,noexec
+devpts                  /dev/pts        devpts  x-create=dir,rw,nosuid,noexec,newinstance,ptmxmode=0666,mode=620
 /dev/tty                /dev/tty        none    x-create=file,bind,rw,nosuid,noexec
-/dev/console            /dev/console    none    x-create=file,bind,rw,nosuid,noexec
 /dev/shm                /dev/shm        none    x-create=dir,bind,rw,nosuid,noexec,nodev
 /dev/mqueue             /dev/mqueue     none    x-create=dir,bind,rw,nosuid,noexec,nodev
 /dev/hugepages          /dev/hugepages  none    x-create=dir,bind,rw,nosuid,noexec,nodev,nofail,silent
 /dev/log                /dev/log        none    x-create=file,bind,rw,nosuid,noexec,nodev
 EOF
 
+if [ -t 0 ]; then
+    enroot-mount --root "${ENROOT_ROOTFS}" - <<< "$(common::realpath /dev/stdin) /dev/console none x-create=file,bind,rw,nosuid,noexec"
+fi
+
 ln -s "/proc/self/fd" "${ENROOT_ROOTFS}/dev/fd"
+ln -s "/dev/pts/ptmx" "${ENROOT_ROOTFS}/dev/ptmx"
 ln -s "/proc/self/fd/0" "${ENROOT_ROOTFS}/dev/stdin"
 ln -s "/proc/self/fd/1" "${ENROOT_ROOTFS}/dev/stdout"
 ln -s "/proc/self/fd/2" "${ENROOT_ROOTFS}/dev/stderr"
