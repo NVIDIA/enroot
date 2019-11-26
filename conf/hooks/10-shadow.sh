51dd4839d8a1735e94e2b34f561b9e34775fa1ac
Jonathan Calmels <jcalmels@nvidia.com>
2019-11-26 14:39:04 -0800

    Improve the shadow hook to support parallel updates

--- conf/hooks/10-shadow.sh
+++ conf/hooks/10-shadow.sh
@@ -31,11 +31,11 @@ readonly grpent=$(common::getgrent)
 # Load the default shadow settings.
 if [ -f "${ENROOT_ROOTFS}/etc/login.defs" ]; then
     # shellcheck disable=SC2046
-    readonly $(awk '(NF && $1 !~ "^#"){ print "def_"$1"="$2 }' "${ENROOT_ROOTFS}/etc/login.defs")
+    declare $(awk '(NF && $1 !~ "^#"){ print "def_"$1"="$2 }' "${ENROOT_ROOTFS}/etc/login.defs")
 fi
 if [ -f "${ENROOT_ROOTFS}/etc/default/useradd" ]; then
     # shellcheck disable=SC2046
-    readonly $(awk '(NF && $1 !~ "^#"){ print "def_"$1 }' "${ENROOT_ROOTFS}/etc/default/useradd")
+    declare $(awk '(NF && $1 !~ "^#"){ print "def_"$1 }' "${ENROOT_ROOTFS}/etc/default/useradd")
 fi
 
 # Read the user/group database entries for the current user on the host.
@@ -48,48 +48,40 @@ if [ ! -x "${ENROOT_ROOTFS}${shell}" ]; then
     shell="${def_SHELL:-/bin/sh}"
 fi
 
-# Create new database files based on the ones present in the rootfs.
-cp -a "${ENROOT_ROOTFS}/etc/passwd" "${ENROOT_ROOTFS}/etc/passwd-"
-cp -a "${ENROOT_ROOTFS}/etc/group" "${ENROOT_ROOTFS}/etc/group-"
+touch "${ENROOT_ROOTFS}/etc/passwd" "${ENROOT_ROOTFS}/etc/group"
+pwddb=$(mktemp -p "${ENROOT_ROOTFS}/etc" .passwd.XXXXXX)
+grpdb=$(mktemp -p "${ENROOT_ROOTFS}/etc" .group.XXXXXX)
+trap "rm -f '${pwddb}' '${grpdb}'" EXIT
 
-# XXX On debian based distributions, remove the _apt user/group otherwise apt will try to drop privileges and fail.
-if [ -f "${ENROOT_ROOTFS}/etc/debian_version" ]; then
-    sed -i '/^_apt:/d' "${ENROOT_ROOTFS}/etc/passwd-"
-    sed -i '/^_apt:/d' "${ENROOT_ROOTFS}/etc/group-"
-fi
-
-# Generate user entries for root, nobody and the current user.
-cat << EOF >> "${ENROOT_ROOTFS}/etc/passwd-"
+# Generate passwd and group entries for root, nobody and the current user.
+# XXX Remove the _apt user/group otherwise apt will try to drop privileges and fail.
+sed '/^_apt:/d' "${ENROOT_ROOTFS}/etc/passwd" - << EOF >> "${pwddb}"
 root:x:0:0:root:/root:${shell}
 nobody:x:${nobody}:${nogroup}:nobody:/:/sbin/nologin
 ${user}:x:${uid}:${gid}:${gecos}:${home}:${shell}
 EOF
 
-# Generate group entries for root, nobody and the current user.
-cat << EOF >> "${ENROOT_ROOTFS}/etc/group-"
+sed '/^_apt:/d' "${ENROOT_ROOTFS}/etc/group" - << EOF >> "${grpdb}"
 root:x:0:
 nogroup:x:${nogroup}:
 ${group}:x:${gid}:
 EOF
 
-# Check and install the new group database making sure our generated groups come first.
-yes 2> /dev/null | grpck -R "${ENROOT_ROOTFS}" /etc/group- > /dev/null 2>&1 || :
-tail -n -3 "${ENROOT_ROOTFS}/etc/group-" > "${ENROOT_ROOTFS}/etc/group"
-head -n -3 "${ENROOT_ROOTFS}/etc/group-" >> "${ENROOT_ROOTFS}/etc/group"
+# Check and install the new databases making sure our generated entries come first.
+yes 2> /dev/null | grpck -R "${ENROOT_ROOTFS}" "${grpdb#${ENROOT_ROOTFS}}" /etc/gshadow > /dev/null 2>&1 || :
+{ tail -n -3 "${grpdb}"; head -n -3 "${grpdb}"; } > "${grpdb}-" && mv "${grpdb}-" "${grpdb}"
+mv "${grpdb}" "${ENROOT_ROOTFS}/etc/group"
 
-# Check and install the new user database making sure our generated users come first.
-yes 2> /dev/null | pwck -R "${ENROOT_ROOTFS}" /etc/passwd- > /dev/null 2>&1 || :
-tail -n -3 "${ENROOT_ROOTFS}/etc/passwd-" > "${ENROOT_ROOTFS}/etc/passwd"
-head -n -3 "${ENROOT_ROOTFS}/etc/passwd-" >> "${ENROOT_ROOTFS}/etc/passwd"
-
-rm -f "${ENROOT_ROOTFS}/etc/group-"* "${ENROOT_ROOTFS}/etc/passwd-"*
+yes 2> /dev/null | pwck -R "${ENROOT_ROOTFS}" "${pwddb#${ENROOT_ROOTFS}}" /etc/shadow > /dev/null 2>&1 || :
+{ tail -n -3 "${pwddb}"; head -n -3 "${pwddb}"; } > "${pwddb}-" && mv "${pwddb}-" "${pwddb}"
+mv "${pwddb}" "${ENROOT_ROOTFS}/etc/passwd"
 
 # Create the user home directory if it doesn't exist and populate it with the content of the skeleton directory.
 if [ ! -e "${ENROOT_ROOTFS}${home}" ] && [ "${def_CREATE_HOME:-yes}" = "yes" ]; then
     ( umask "${def_UMASK:-077}" && mkdir -p "${ENROOT_ROOTFS}${home}" )
     skel="${def_SKEL:-/etc/skel}"
     if [ -d "${ENROOT_ROOTFS}${skel}" ]; then
-        cp -a "${ENROOT_ROOTFS}${skel}/." "${ENROOT_ROOTFS}${home}"
+        cp -Lpr "${ENROOT_ROOTFS}${skel}/." "${ENROOT_ROOTFS}${home}"
     fi
 fi
 
