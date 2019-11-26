260bb42a47fc4a200b44494d7727453aff574fcb
Jonathan Calmels <jcalmels@nvidia.com>
2019-11-26 14:48:04 -0800

    Set HOME to /root when ENROOT_MOUNT_HOME and ENROOT_REMAP_ROOT are set

--- conf/hooks/10-home.sh
+++ conf/hooks/10-home.sh
@@ -31,6 +31,7 @@ fi
 
 if [ -n "${ENROOT_REMAP_ROOT-}" ]; then
     enroot-mount --root "${ENROOT_ROOTFS}" - <<< "${HOME} /root none x-create=dir,bind,rw,nosuid"
+    printf "HOME=/root\n" >> "${ENROOT_ENVIRON}"
 else
     enroot-mount --root "${ENROOT_ROOTFS}" - <<< "${HOME} ${HOME} none x-create=dir,bind,rw,nosuid"
     printf "HOME=%s\n" "${HOME}" >> "${ENROOT_ENVIRON}"
