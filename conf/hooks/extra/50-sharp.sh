5a4c700a21f1428fa17b721e11d000ed0774dc95
Felix Abecassis <fabecassis@nvidia.com>
2025-09-26 15:42:14 -0700

    Remove sharpd handling from hook 50-sharp.sh
    Signed-off-by: Felix Abecassis <fabecassis@nvidia.com>

--- conf/hooks/extra/50-sharp.sh
+++ conf/hooks/extra/50-sharp.sh
@@ -29,20 +29,8 @@ if [ "${MELLANOX_VISIBLE_DEVICES:-void}" = "void" ] || [ "${MELLANOX_VISIBLE_DEV
     exit 0
 fi
 
-sharp_sock=$(awk '($8 ~ "^@sharpd" && $8 != "@sharpd_unix_sock"){print $8}' /proc/net/unix)
-
-# The sharpd daemon has been removed in NVIDIA SHARP 2.7.0:
-# https://docs.nvidia.com/networking/display/SHARPv270/Running+NVIDIA+SHARP+Aggregation+Manager+%28AM%29+Daemons
-# To trigger this hook when sharpd is not present, we check whether --network=sharp was used in the Slurm job.
-#
-# SLURM_NETWORK is not set consistently (https://bugs.schedmd.com/show_bug.cgi?id=14336), so a patch is
-# currently needed: https://gitlab.com/nvidia/dlsyseng/slurm/-/commit/8060b224960e5cd036f3eae881de42eccd40d399
-if [ -z "${sharp_sock}" ] && [ "${SLURM_NETWORK:-}" != "sharp" ]; then
+if [ "${SLURM_NETWORK:-}" != "sharp" ]; then
     exit 0
-fi
-
-if [ -n "${sharp_sock}" ]; then
-   printf "SHARP_COLL_SHARPD_SOCKET_NAME=%s\n" "${sharp_sock#@}" >> "${ENROOT_ENVIRON}"
 fi
 
 # Disable SHARP lazy locking and initialization.
