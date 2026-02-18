57b5752b28eb183aa07929d21d74461214b382fc
Felix Abecassis <fabecassis@nvidia.com>
2026-02-17 20:15:33 -0800

    Fix "enroot remove" failing on directories with restricted permissions
    Reported-by: Mathew Wicks <mwicks@nvidia.com>
    Signed-off-by: Felix Abecassis <fabecassis@nvidia.com>

--- src/common.sh
+++ src/common.sh
@@ -69,7 +69,7 @@ common::rmall() {
     local -r path="$1"
 
     rm --one-file-system --preserve-root -rf "${path}" 2> /dev/null || \
-    { chmod -f -R +wX "${path}" || :; rm --one-file-system --preserve-root -rf "${path}"; }
+    { chmod -f -R +rwX "${path}" || :; rm --one-file-system --preserve-root -rf "${path}"; }
 }
 
 common::mktmpdir() {
